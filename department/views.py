0ba1c513e6c58c9c70568ea79b9efba9e4d4f7f7
Pemmasi1 <sivaanjaneyulupemmasani@gmail.com>
2025-03-06 19:59:41 -0500

    Add logging to DepartmentViewSet for better traceability

--- department/views.py
+++ department/views.py
@@ -1,7 +1,11 @@
+import logging
 from rest_framework import viewsets
 from .models import Department
 from .serializers import DepartmentSerializer
 
+logger = logging.getLogger(__name__)
+
 class DepartmentViewSet(viewsets.ModelViewSet):
+    logger.info('DepartmentViewSet')
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
